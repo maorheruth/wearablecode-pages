704f3d878153f162b725feb9efab6f8d98af8f5d
maorheruth <maorheruth@gmail.com>
2025-05-28 15:09:15 +0300

    Update shopify.js

--- wearablecode-pages/api/webhooks/shopify.js
+++ wearablecode-pages/api/webhooks/shopify.js
@@ -1,4 +1,4 @@
-import { db } from '../../../firebase-config.js';
+import { db } from '../../firebase-config.js';
 import { collection, addDoc } from 'firebase/firestore';
 
 export default async function handler(req, res) {
